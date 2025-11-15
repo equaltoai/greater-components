e1618b99ea8134c012bb1376e7f9c652ebe10bd9
Aron Price <aron23@gmail.com>
2025-11-14 19:55:45 -0500

    more fixes

--- packages/testing/tests/demo/primitives.spec.ts
+++ packages/testing/tests/demo/primitives.spec.ts
@@ -4,9 +4,7 @@ test.describe('Primitives demo', () => {
 	test.beforeEach(async ({ page }) => {
 		await page.goto('/demos/primitives');
 		await page.waitForLoadState('networkidle');
-		await page.waitForFunction(() =>
-			document.documentElement.hasAttribute('data-sveltekit-hydrated')
-		);
+		await page.waitForSelector('[data-sveltekit-hydrated]', { state: 'attached' });
 	});
 
 	test('button interactions update click counter', async ({ page }) => {
